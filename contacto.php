1d276bca666e902de2787868463f2b8656c9453f
Desde OFICINA EBALS <roycarisarmiento@gmail.com>
2022-05-16 13:56:54 -0500

    se realizaron camio y agregó el boton de whatsapp

--- contacto.php
+++ contacto.php
@@ -33,9 +33,9 @@
 
     <!-- Resumen de Servicio-->
     <section class="section section-sm section-fluid bg-default text-center" id="projects">
-        <div class="container-fluid">
+      <div class="container-fluid">
         <div class="row row-30 justify-content-center">
-            <div class="col-lg-8 col-xl-8">
+         <div class="col-lg-8 col-xl-8">
             <article class="title-classic">
             <div class="title-classic-title">
               <h3>SOLICITA TU COTIZACIÓN</h3>
@@ -43,78 +43,78 @@
             <div class="title-classic-text">
               <p>Llena nuestro formulario para enviarte una propuesta detallada del servicio.</p>
             </div>
-          </article>
-            <form class="rd-form rd-form-variant-2 rd-mailform" data-form-output="form-output-global" data-form-type="contact" method="post" action="bat/rd-mailform.php">
-            <div class="row row-14 gutters-14">
-              <div class="col-md-4">
-                <div class="form-wrap">
-                  <input class="form-input" id="contact-your-name-2" type="text" name="name" data-constraints="@Required">
-                  <label class="form-label" for="contact-your-name-2">Nombres</label>
+            </article>
+                <form class="rd-form rd-form-variant-2 rd-mailform" data-form-output="form-output-global" data-form-type="contact" method="post" action="bat/rd-mailform.php">
+                  <div class="row row-14 gutters-14">
+                    <div class="col-md-4">
+                      <div class="form-wrap">
+                        <input class="form-input" id="contact-your-name-2" type="text" name="name" data-constraints="@Required">
+                        <label class="form-label" for="contact-your-name-2">Nombres</label>
+                      </div>
+                    </div>
+                    <div class="col-md-4">
+                      <div class="form-wrap">
+                        <input class="form-input" id="contact-email-2" type="email" name="email" data-constraints="@Email @Required">
+                        <label class="form-label" for="contact-email-2">Email</label>
+                      </div>
+                    </div>
+                    <div class="col-md-4">
+                      <div class="form-wrap">
+                        <input class="form-input" id="contact-phone-2" type="text" name="phone" data-constraints="@Numeric">
+                        <label class="form-label" for="contact-phone-2">Celular</label>
+                      </div>
+                    </div>
+                    <div class="col-12">
+                      <div class="form-wrap">
+                        <label class="form-label" for="contact-message-2">Mensaje</label>
+                        <textarea class="form-input textarea-lg" id="contact-message-2" name="message" data-constraints="@Required"></textarea>
+                      </div>
+                    </div>
+                  </div>
+                  <button class="button button-primary button-pipaluk" type="submit">Enviar</button>
+                </form>
+          </div>
+          <div class="col-md-4 col-lg-4 col-xl-4 text-lg-center wow fadeInUp">
+                <div class="container">
+                  <div class="row row-30 justify-content-center">
+                    <div class="col-sm-12 col-md-12 col-lg-12 caja-contacto">
+                      <h3>CELULAR</h3>
+                      <article class="contacto-estilo">
+                        
+                        <div class="box-contacts-icon-fila fl-bigmug-line-cellphone55"></div>
+                        <div class="texto-contacto">
+                          <p class="box-contacts-link"><a href="tel:#"> +1 323-913-4688</a> / <a href="tel:#">+1 323-888-4554</a></p>
+                          
+                        </div>
+                        
+                      </article>
+                    </div>
+                    <div class="col-sm-12 col-md-12 col-lg-12 caja-contacto">
+                      <h3>DIRECCIÓN</h3>
+                      <article class="contacto-estilo">
+                        <div class="box-contacts-icon-fila fl-bigmug-line-up104"></div>
+                        <div class="texto-contacto">
+                          <p class="box-contacts-link"><a href="#">Av. Las Camelias 790, distrito de San Isidro</a></p>
+                        </div>
+                      </article>
+                    </div>
+                    <div class="col-sm-12 col-md-12 col-lg-12 caja-contacto">
+                      <h3>CORREO ELECTRÓNICO</h3>
+                      <article class="contacto-estilo">
+                        <div class="box-contacts-icon-fila fl-bigmug-line-chat55"></div>
+                        <div class="texto-contacto">
+                          <p class="box-contacts-link"><a href="mailto:#">ebelin@ebalsconsultores.com</a></p>
+                          
+                        </div>
+                      </article>
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div class="col-md-4">
-                <div class="form-wrap">
-                  <input class="form-input" id="contact-email-2" type="email" name="email" data-constraints="@Email @Required">
-                  <label class="form-label" for="contact-email-2">Email</label>
-                </div>
-              </div>
-              <div class="col-md-4">
-                <div class="form-wrap">
-                  <input class="form-input" id="contact-phone-2" type="text" name="phone" data-constraints="@Numeric">
-                  <label class="form-label" for="contact-phone-2">Celular</label>
-                </div>
-              </div>
-              <div class="col-12">
-                <div class="form-wrap">
-                  <label class="form-label" for="contact-message-2">Mensaje</label>
-                  <textarea class="form-input textarea-lg" id="contact-message-2" name="message" data-constraints="@Required"></textarea>
-                </div>
-              </div>
-            </div>
-            <button class="button button-primary button-pipaluk" type="submit">Enviar</button>
-          </form>
-            </div>
-            <div class="col-md-4 col-lg-4 col-xl-4 text-lg-center wow fadeInUp">
-            <div class="row row-30 justify-content-center">
-            <div class="col-sm-12 col-md-12 col-lg-12">
-            <div class="atf-contact-info mx-auto my-auto">
-									<div class="atf-contact-details">
-										<i class="fa-solid fa-location-dot"></i>
-										<h4>Dirección de oficina.</h4>
-										<p>Av. Las Camelias 790, distrito de San Isidro</p>
-									</div>
-									<div class="atf-contact-details">
-										<i class="fa-solid fa-phone"></i>
-										<h4>Celular</h4>
-										<p>(+51)957404672 / (+51)981453402</p>
-									</div>
-									<div class="atf-contact-details">
-										<i class="fa fa-envelope" aria-hidden="true"></i>
-										<h4>Correo Electrónico</h4>
-										<p>ebelin@zuimedic.com</p>
-									</div>
-								</div>
-                    <h4>CELULAR</h4>
-                    <p class="box-contacts-link"><a href="tel:#">+1 323-913-4688</a></p>
-                    <p class="box-contacts-link"><a href="tel:#">+1 323-888-4554</a></p>
-                    
-                    <h4>EMAIL</h4>
-                    <p class="box-contacts-link"><a href="mailto:ebelin@ebalsconsultores.com">ebelin@ebalsconsultores.com</a></p>
-                    
-                    <h4>OFICINA</h4>
-                    <p class="box-contacts-link">Calle Los Crisantemos N° 162 Of. 502 - Urb. Los Recaudadores Ate - Lima</p>
-                    </br>
-                </div>
-
-
           </div>
 
-
-            </div>
-
         </div>
 
-        </div>
+      </div>
     </section>
 
 
@@ -140,5 +140,8 @@
     <script src="js/core.min.js"></script>
     <script src="js/script.js"></script>
     <!-- coded by Himic-->
+    <a href="https://api.whatsapp.com/send?phone=51989032838" class="btn-wsp btn-wsp " target="_blank">
+    <i class="icon fa-whatsapp"></i>
+    </a>
   </body>
 </html>
